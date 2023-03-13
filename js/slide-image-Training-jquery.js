f87b4bd81a2062ee774977fc22646d447321650c
Yang Hee Tae <87000854+gmlxo@users.noreply.github.com>
2023-03-13 09:39:45 +0900

    slide Training

--- js/slide-image-Training-jquery.js
+++ js/slide-image-Training-jquery.js
@@ -1,30 +1,28 @@
 var slide = $(".slide>img");
 var sno = 0;
 var eno = slide.length - 1;
-var timer = setInterval("autosilde()", 3000); 
+var timer = setInterval("autosilde()", 2000); 
 
 $(".slide").hover(function () {
         // over
         clearInterval(timer)
     }, function () {
         // out
-        timer = setInterval("autosilde()", 3000);
+        timer = setInterval("autosilde()", 2000);
     }
 );
 
 function autosilde() {
     $(slide[sno]).stop().animate({
-        // left:"100%"
         right: "100%"
-    }, 1000, function () {
-        $(this).css({ /* left: "-100%" */ right: "-100%" });
+    }, 500, function () {
+        $(this).css({ right: "-100%" });
     });
     sno++;
     if (sno > eno) {
         sno = 0;
     }
     $(slide[sno]).stop().animate({
-        // left: "0"
         right: "0"
-    }, 1000)
+    }, 500)
 }
