b3098f46980dab5fbd0db0f5f4399542549d07e2
Yang Hee Tae <87000854+gmlxo@users.noreply.github.com>
2023-03-20 12:45:27 +0900

    B-4

--- js/B-4-jquery.js
+++ js/B-4-jquery.js
@@ -26,32 +26,6 @@ function autosilde() {
     }, 1000)
 }
 
-$(".tab-news").click(function () {
-    $(this).css({
-        "border-bottom": "0",
-        "background-color": "#fff"
-    });
-    $(".tab-gallery").css({
-        "border-bottom": "1px solid #000",
-        "background-color": "#888"
-    });
-    $(".gallery").hide();
-    $(".news").show();
-});
-
-$(".tab-gallery").click(function () {
-    $(this).css({
-        "border-bottom": "0",
-        "background-color": "#fff"
-    });
-    $(".tab-news").css({
-        "border-bottom": "1px solid #000",
-        "background-color": "#888"
-    });
-    $(".news").hide();
-    $(".gallery").show();
-});
-
 function openPop() {
     $("#pop").show("show");
 }
