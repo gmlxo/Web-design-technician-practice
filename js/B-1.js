3f3e176b812770e503e2928d4759e5fe20381bfe
gmlxo <87000854+gmlxo@users.noreply.github.com>
2023-05-01 21:54:04 +0900

    b 1
    처음 부터 다시 해본 결과
    시험 시간이 4시간이라는 과정하에 1시간 반 정도 남는다

--- js/B-1.js
+++ js/B-1.js
@@ -1,25 +1,14 @@
-$("ul").hover(
-    function() {
-        $("ul.sub").stop().slideDown();
-    }, function() {
-        $("ul.sub").stop().slideUp();
-    }
-);
-
-var slide = $("div.slide>div");
-var n = 0;
-var num = slide.length - 1;
-var time = setInterval("autoslide()", 3000);
-
+$(".menu-box").hover(function() { $(".menu-box>ul>li>ul").stop().slideDown();}, function() { $(".menu-box>ul>li>ul").stop().slideUp(); });
+var slide = $(".slide>div"), num = slide.length-1, n=0, time = setInterval("autoslide()", 3000);
 function autoslide() {
-    $(slide[n]).animate({
-        left: "100%"
-    }, 1000, function() {
-        $(this).css({ left: "-100% "});
-    });
-    n++;
-    if(n > num) n = 0;
-    $(slide[n]).animate({
-        left: "0"
-    }, 1000);
+    $(slide[n]).animate({ left: "-100%" }, 1000, function() { $(this).css({ left: "100%"}); }), 1000 
+    n++; if(n>num) n=0; $(slide[n]).animate({ left: "0" }, 1000); 
 }
+// 찾아봄 _ 현 잘 모르는 거
+$(slide).hover(function() { clearInterval(time); }, function() { time = setInterval("autoslide()", 3000); });
+
+$(".n-t").click(function() { $(".n-t").css({ "background-color": "#ccccff"}); $(".g-t").css({ "background-color": "#fff"}); $(".n-box").css({ "background-color": "#ccccff"}); $(".n-box").show(); $(".g-box").hide(); });
+$(".g-t").click(function() { $(".g-t").css({ "background-color": "#ccccff"}); $(".n-t").css({ "background-color": "#fff"}); $(".g-box").css({ "background-color": "#ccccff"}); $(".g-box").show(); $(".n-box").hide(); });
+
+function pop() { $("#pop").show(); }
+function popClose() { $("#pop").hide(); }
